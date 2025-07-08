c561513a92b55e2cd6551f51f0f0a135ca385045
surya omar <suryaomar323@gmail.com>
2025-07-08 14:21:40 +0530

    Update 112.path sum.py

--- 112.path sum.py
+++ 112.path sum.py
@@ -9,3 +9,19 @@ class Solution:
             return targetSum == root.val
         return (self.hasPathSum(root.left, targetSum - root.val) or
                 self.hasPathSum(root.right, targetSum - root.val))
+
+
+using helper 
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        def helper(x,target,sum):
+            if not x:
+                return False
+            if x and not x.left and not x.right:
+                sum=sum+x.val
+            if sum==target:
+                return True
+            return helper(x.left,target,sum+x.val) or helper(x.right,target,sum+x.val)
+            return helper(root,targetsum,0)
+   
+
