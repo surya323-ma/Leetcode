9ea182f3355901f1fc055de3f6aa9fd5d8d8e661
surya omar <suryaomar323@gmail.com>
2025-07-18 20:23:00 +0530

    Create 450. Delete Node in a BST.py

---
+++ 450. Delete Node in a BST.py
@@ -0,0 +1,34 @@
+Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
+
+Basically, the deletion can be divided into two stages:
+
+Search for a node to remove.
+If the node is found, delete the node.
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if not root.left:
+                return root.right
+            elif not root.right:
+                return root.left
+            else:
+                successor = root.right
+                while successor.left:
+                    successor = successor.left
+                root.val = successor.val
+                root.right = self.deleteNode(root.right, successor.val)
+
+        return root
