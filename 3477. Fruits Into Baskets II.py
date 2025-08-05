3115c2563f4df16b59df8035633ac47126f566c5
surya omar <suryaomar323@gmail.com>
2025-08-05 12:08:53 +0530

    Create 3477. Fruits Into Baskets II.py

---
+++ 3477. Fruits Into Baskets II.py
@@ -0,0 +1,21 @@
+You are given two arrays of integers, fruits and baskets, each of length n, where fruits[i] represents the quantity of the ith type of fruit, and baskets[j] represents the capacity of the jth basket.
+
+From left to right, place the fruits according to these rules:
+
+Each fruit type must be placed in the leftmost available basket with a capacity greater than or equal to the quantity of that fruit type.
+Each basket can hold only one type of fruit.
+If a fruit type cannot be placed in any basket, it remains unplaced.
+Return the number of fruit types that remain unplaced after all possible allocations are made.
+
+class Solution:
+    def numOfUnplacedFruits(self, fruits: List[int], baskets: List[int]) -> int:
+        used = set()
+        placed = 0
+        for i in range(len(fruits)):
+            for j in range(len(baskets)):
+                if fruits[i]<=baskets[j] and j not in used:
+                    used.add(j)
+                    placed+=1
+                    break
+        return len(fruits)-placed
+        
