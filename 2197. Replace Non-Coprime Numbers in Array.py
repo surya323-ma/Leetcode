a13eec67c04299d20357598b1126a1d2abc927ba
surya omar <suryaomar323@gmail.com>
2025-09-16 13:08:30 +0530

    Create 2197. Replace Non-Coprime Numbers in Array.py

---
+++ 2197. Replace Non-Coprime Numbers in Array.py
@@ -0,0 +1,24 @@
+You are given an array of integers nums. Perform the following steps:
+
+Find any two adjacent numbers in nums that are non-coprime.
+If no such numbers are found, stop the process.
+Otherwise, delete the two numbers and replace them with their LCM (Least Common Multiple).
+Repeat this process as long as you keep finding two adjacent non-coprime numbers.
+Return the final modified array. It can be shown that replacing adjacent non-coprime numbers in any arbitrary order will lead to the same result.
+
+The test cases are generated such that the values in the final array are less than or equal to 108.
+
+Two values x and y are non-coprime if GCD(x, y) > 1 where GCD(x, y) is the Greatest Common Divisor of x and y.
+
+ from math import gcd
+
+def lcm(x, y):
+    return x * y // gcd(x, y)
+class Solution:
+    def replaceNonCoprimes(self, nums: List[int]) -> List[int]:
+        stack = []
+        for num in nums:
+            while stack and gcd(stack[-1], num) > 1:
+                num = lcm(stack.pop(), num)
+            stack.append(num)
+        return stack
