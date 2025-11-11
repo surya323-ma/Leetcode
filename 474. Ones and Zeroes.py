5b42662c7663653d21392d74a3fae0b2ab5c9d54
surya omar <suryaomar323@gmail.com>
2025-11-11 11:54:35 +0530

    Add solution for 'Ones and Zeroes' problem
    Implement a solution to find the largest subset of binary strings with constraints on the number of 0's and 1's.

---
+++ 474. Ones and Zeroes.py
@@ -0,0 +1,16 @@
+You are given an array of binary strings strs and two integers m and n.
+Return the size of the largest subset of strs such that there are at most m 0's and n 1's in the subset.
+A set x is a subset of a set y if all elements of x are also elements of y.
+class Solution:
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        for s in strs:
+            zeros = s.count('0')
+            ones = s.count('1')
+
+        # Traverse dp table in reverse to avoid overwriting needed values
+            for i in range(m, zeros - 1, -1):
+                for j in range(n, ones - 1, -1):
+                    dp[i][j] = max(dp[i][j], dp[i - zeros][j - ones] + 1)
+
+        return dp[m][n]
