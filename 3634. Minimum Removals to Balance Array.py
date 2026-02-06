3a4ff5815ff135b4d3379f6b3f452666b1c4eca1
surya omar <suryaomar323@gmail.com>
2026-02-06 21:02:43 +0530

    Create 3634. Minimum Removals to Balance Array.py

---
+++ 3634. Minimum Removals to Balance Array.py
@@ -0,0 +1,21 @@
+You are given an integer array nums and an integer k.
+
+An array is considered balanced if the value of its maximum element is at most k times the minimum element.
+
+You may remove any number of elements from nums​​​​​​​ without making it empty.
+
+Return the minimum number of elements to remove so that the remaining array is balanced.
+
+Note: An array of size 1 is considered balanced as its maximum and minimum are equal, and the condition always holds true.
+
+ class Solution:
+    def minRemoval(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        n = len(nums)
+        i = 0
+        max_len = 1 
+        for j in range(n):
+            while nums[j] > k * nums[i]:
+                i += 1
+            max_len = max(max_len, j - i + 1)
+        return n - max_len
