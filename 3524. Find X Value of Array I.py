e001e3193ee983ef4e65071e391ac940884b5f6b
surya omar <suryaomar323@gmail.com>
2026-09-21 18:53:00 +0530

    Add solution for finding x-value of array
    Implement a function to calculate the x-value of an array based on product remainders.

---
+++ 3524. Find X Value of Array I.py
@@ -0,0 +1,27 @@
+You are given an array of positive integers nums, and a positive integer k.
+You are allowed to perform an operation once on nums, where in each operation you can remove any non-overlapping prefix and suffix from nums such that nums remains non-empty.
+You need to find the x-value of nums, which is the number of ways to perform this operation so that the product of the remaining elements leaves a remainder of x when divided by k.
+Return an array result of size k where result[x] is the x-value of nums for 0 <= x <= k - 1.
+A prefix of an array is a subarray that starts from the beginning of the array and extends to any point within it.
+A suffix of an array is a subarray that starts at any point within the array and extends to the end of the array.
+Note that the prefix and suffix to be chosen for the operation can be empty.
+
+class Solution:
+    def resultArray(self, nums: List[int], k: int) -> List[int]:
+        ans = [0] * k
+        dp = [0] * k
+
+        for num in nums:
+            x = num % k
+            nxt = [0] * k
+            nxt[x] += 1
+
+            for r in range(k):
+                new_r = (r * x) % k
+                nxt[new_r] += dp[r]
+
+            for r in range(k):
+                ans[r] += nxt[r]
+            dp = nxt
+
+        return ans                                            
